5fe288c251ce6a870e05200f9818962d63c18533
Fabio Malizia <malizia.fabio@iisgalvanimi.edu.it>
2022-05-20 14:18:49 +0000

    Primo test heroku

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # basic-docker-heroku-ci
+
+- install heroku: npm install -g heroku
+- heroku login -i
---
+++ api-service/app.py
@@ -0,0 +1,33 @@
+import flask
+from flask import jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+books = [
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Simple api test</h1><p>Type /api/allbooks on the URL to get some data</p>"
+
+@app.route('/api/allbooks', methods=['GET'])
+def api():
+    return jsonify(books)
+
+app.run(host="0.0.0.0", port=5000)
